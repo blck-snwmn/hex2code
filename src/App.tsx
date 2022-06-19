1538186f9cd72bf07e4a1b4cc7d8e93949d82156
blck-snwmn <whitesnowmancreator@gmail.com>
2022-06-19 19:15:41 +0900

    Use deno fmt

--- src/App.tsx
+++ src/App.tsx
@@ -1,67 +1,84 @@
-import { useReducer, useState } from 'react'
+import { useReducer, useState } from 'react';
 import {
+  Box,
+  Button,
+  Container,
+  Divider,
   FormLabel,
+  Grid,
+  GridItem,
   NumberInput,
   NumberInputField,
   Textarea,
-  Divider,
-  Button,
-  Container,
-  Grid,
-  GridItem,
-} from '@chakra-ui/react'
+} from '@chakra-ui/react';
 
-type Action = { language: 'Go', input: string, word: number }
+type Action = { language: 'Go'; input: string; word: number };
 
-const mapper = {
-}
+const mapper = {};
 
 const reducer = (_: string, action: Action) => {
-  const input = action.input
-  const split = input.split(' ')
-  const filltered = split.filter(i => i.length == 2)
-  const mapped = filltered.map(i => '0x' + i + ',') // to hex format
+  const input = action.input;
+  const split = input.split(' ');
+  const filltered = split.filter((i) => i.length == 2);
+  const mapped = filltered.map((i) => '0x' + i + ','); // to hex format
 
   const format = mapped.reduce((acc, x) => { // fix the length of one line
-    const last = acc.length - 1
-    acc[last].push(x)
+    const last = acc.length - 1;
+    acc[last].push(x);
     if (acc[last].length == action.word) {
-      acc.push([]) // new line
+      acc.push([]); // new line
     }
-    return acc
-  }, [[]] as [string[]])
-  const joined = format.map(i => i.join(' ')).join('\n')
-  return joined
-}
+    return acc;
+  }, [[]] as [string[]]);
+  const joined = format.map((i) => i.join(' ')).join('\n');
+  return joined;
+};
 
 function App() {
-  const [value, setValue] = useState("")
-  const [wordNum, setWordNum] = useState("")
-  const [state, dispatch] = useReducer(reducer, "")
+  const [value, setValue] = useState('');
+  const [wordNum, setWordNum] = useState('');
+  const [state, dispatch] = useReducer(reducer, '');
 
   return (
-    <Container maxW='100rem' >
-      <Grid templateAreas={`"left right"`}
+    <Container maxW='100rem'>
+      <Grid
+        templateAreas={`"left right"`}
         gridTemplateColumns={'1fr 1fr'}
         gridTemplateRows={'1fr'}
-        gap={6}>
+        gap={6}
+      >
         <GridItem area={'left'}>
-          <Textarea rows={20} cols={100} onChange={(e) => { setValue(e.target.value) }} ></Textarea>
-          <FormLabel htmlFor="wordPerLint">1行の文字数</FormLabel >
+          <Textarea
+            rows={20}
+            cols={100}
+            onChange={(e) => {
+              setValue(e.target.value);
+            }}
+          >
+          </Textarea>
+          <FormLabel htmlFor='wordPerLint'>1行の文字数</FormLabel>
           <NumberInput name='wordPerLint'>
-            <NumberInputField onChange={(e) => { setWordNum(e.target.value) }} />
+            <NumberInputField
+              onChange={(e) => {
+                setWordNum(e.target.value);
+              }}
+            />
           </NumberInput>
-          <Button onClick={() => {
-            const w = Number(wordNum)
-            dispatch({ language: "Go", input: value, word: w ? w : 8 })
-          }}>Change</Button>
+          <Button
+            onClick={() => {
+              const w = Number(wordNum);
+              dispatch({ language: 'Go', input: value, word: w ? w : 8 });
+            }}
+          >
+            Change
+          </Button>
         </GridItem>
         <GridItem area={'right'}>
           <Textarea rows={20} cols={200} value={state}></Textarea>
         </GridItem>
       </Grid>
     </Container>
-  )
+  );
 }
 
-export default App
+export default App;
